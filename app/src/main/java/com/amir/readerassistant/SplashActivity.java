034aba206a755bdb832a2951c7a7e80678a953f3
amirebrahim hashemi <hashemi.amirebrahim@gmail.com>
2024-03-11 15:34:59 +0330

    SplashScreen Improvement

--- app/src/main/java/com/amir/readerassistant/SplashActivity.java
+++ app/src/main/java/com/amir/readerassistant/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.amir.readerassistant;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,8 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,18 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                timer.cancel();
+                finish();
+
+            }
+        }, 6000, 1000);
+
     }
 }
